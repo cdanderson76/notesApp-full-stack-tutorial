0873f1b9cac18388c74f1eec68d5919b1ed5b6bd
Cliff Anderson <snappinmambo@gmail.com>
2025-06-04 16:30:18 -0500

    challenge

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router';
+import NoteDetailPage from './pages/NoteDetailPage';
+import CreatePage from './pages/CreatePage';
+import HomePage from './pages/HomePage';
+
+export default function App() {
+
+  return (
+    <div data-theme='forest'>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='/create' element={ <CreatePage /> } />
+        <Route path='/note/:id' element={ <NoteDetailPage /> } />
+      </Routes>
+    </div>
+  )
+}
