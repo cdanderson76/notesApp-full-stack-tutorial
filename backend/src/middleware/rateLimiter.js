d9de6e58094733c457eb2bd06946e7c33cf37813
Cliff Anderson <snappinmambo@gmail.com>
2025-06-10 10:40:09 -0500

    FINISHED

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,14 @@
+import ratelimit from "../../config/upstash.js";
+
+export default async function rateLimiter(req, res, next) {
+  try {
+    const { success } = await ratelimit.limit('my-rate-limit');
+
+    if(!success) {
+      return res.status(429).json({ message: 'Too many requests, please try again later' });
+    }
+    next();
+  } catch(error) {
+    console.error('Rate limit error', error);
+  }
+}
