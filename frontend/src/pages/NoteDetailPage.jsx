35d8a759428f9cc9a9d77d26278c11f3cf03e797
Cliff Anderson <snappinmambo@gmail.com>
2025-06-10 09:39:28 -0500

    challenge

---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,97 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from 'react-router';
+import { Link } from 'react-router-dom';
+import { ArrowLeft, LoaderIcon, Trash2Icon } from 'lucide-react';
+import api from "../../lib/axios";
+
+export default function NoteDetailPage() {
+
+  const [ note, setNote ] = useState(null);
+  const [ loading, setLoading ] = useState(true);
+  const [ saving, setSaving ] = useState(false);
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchNote() {
+      try {
+        const res = await api.get(`/notes/${id}`);
+        setNote(res.data);
+      } catch(error) {
+        console.log(error.message);
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchNote()
+  }, [id]);
+
+  async function handleDelete() {
+
+  }
+
+  async function handleSave() {
+
+  }
+
+  if(loading) {
+    return (
+      <div className="min-h-screen bg-base-200 flex items-center justify-center">
+        <LoaderIcon className='animate-spin size-10' />
+      </div>
+    )
+  }
+
+  return (
+    <div className='min-h-screen bg-base-200'>
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <div className="flex items-center justify-between mb-6">
+            <Link to={'/'} className="btn btn-ghost mb-6">
+              <ArrowLeft className='size-5'/>
+              Back to Notes
+            </Link>
+            <button className="btn btn-error btn-outline">
+              <Trash2Icon className="h-5 w-5"/>
+              Delete Note
+            </button>
+          </div>
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Title</span>
+                </label>
+                <input type="text" 
+                        placeholder="Note Title"
+                        className="input input-bordered"
+                        required
+                        value={note.title}
+                        onChange={(e) => setTitle(e.target.value)} />
+              </div>
+              <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">Content</span>
+              </label>
+              <textarea
+                        placeholder="Write your note here..."
+                        className="textarea textarea-bordered h-32"
+                        required
+                        value={note.content}
+                        onChange={(e) => setContent(e.target.value)} />
+              </div>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary" 
+                        disabled={saving}
+                        onClick={handleSave}>
+                  { saving ? 'Saving...' : 'Save Changes' }
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
